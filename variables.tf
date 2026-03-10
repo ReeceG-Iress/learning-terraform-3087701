34929f05d1ef70e477d2d6a84c4dde38e917fe3c
ReeceG-Iress <reece.gallaway@iress.com>
2026-03-10 16:49:07 +0000

    refactor to use variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,47 @@ variable "instance_type" {
  description = "Type of EC2 instance to provision"
  default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "AMI filter to find the latest AMI for the application"
+  
+  type = object({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  
+  type        = object({
+    name              = string
+    network_prefix    = string
+  })
+
+  default     = {
+    name              = "dev"
+    network_prefix    = "10.0"
+  }
+}
+
+variable "asg_settings" {
+  description = "Auto Scaling Group settings"
+
+  type = object({
+    min_size         = number
+    max_size         = number
+    desired_capacity = number
+  })
+  
+  default = {
+    min_size         = 1
+    max_size         = 2
+    desired_capacity = 1
+  }
+}
